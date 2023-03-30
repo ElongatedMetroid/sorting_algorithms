46dba4449fef9346d832d5e84766ff1751f115e2
ElongatedMetroid <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-03-29 21:24:40 -0700

    changes colors depending on whether the column moves up or down

--- visualization/src/sort_visualize.rs
+++ visualization/src/sort_visualize.rs
@@ -6,8 +6,8 @@ pub struct SortVisualizePlugin;
 impl Plugin for SortVisualizePlugin {
     fn build(&self, app: &mut App) {
         app
-            .insert_resource(SortingVector(vec![5, 4, 3, 2, 1]))
-            .insert_resource(UpdateTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .insert_resource(SortingVector(vec![5, 4, 3, 2, 1, -12, 20, -23, 90, -20, 032, -23, 2, 1, 3, 5, 23, 12, 2]))
+            .insert_resource(UpdateTimer(Timer::from_seconds(0.5, TimerMode::Repeating)))
             .add_startup_system(setup)
             .add_system(update);
     }
@@ -49,10 +49,22 @@ fn update(
     mut sorting_columns: Query<(&mut Sprite, &SortingColumn)>
 ) {
     if timer.0.tick(time.delta()).just_finished() {
-        sorting_vector.0.step();
+        if sorting_vector.0.step() {
+            println!("sorting completed");
+            return;
+        };
         
         for (mut sprite, column) in &mut sorting_columns {
-            sprite.custom_size = Some(Vec2::new(10.0, sorting_vector.0[column.0] as f32 * 10.0));
+            let new_height = sorting_vector.0[column.0] as f32 * 10.0;
+
+            // if the old height was greater than the new height
+            if sprite.custom_size.unwrap().y > new_height {
+                sprite.color = Color::BLUE;
+            } else {
+                sprite.color = Color::RED;
+            }
+
+            sprite.custom_size = Some(Vec2::new(10.0, new_height));
         }
     }
 }
